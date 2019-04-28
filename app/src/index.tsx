36c9819c53f1958f88438fbd944ae5fb482ab3ad
RyotArch <bboy.ryu@icloud.com>
2019-04-28 02:19:39 -0700

    Added Todo List with Redux
    add todo from tutorial code and changed tsconfig

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,10 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
 
-import { Hello } from "./components/Hello";
+const store = createStore(rootReducer)
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
-
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
